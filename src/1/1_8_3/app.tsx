4198d7099966fa7c7a870673573eed8f34ba26a7
AyanValiulin01 <ayangross@outlook.com>
2025-02-22 17:26:20 +0600

    Create app.tsx

---
+++ src/1/1_8_3/app.tsx
@@ -0,0 +1,22 @@
+export interface Story {
+  id: string;
+  label: string;
+} 
+
+const stories: Story[] = [];
+
+export default function Wrapper() {
+  return <StoryTray stories={stories} />;
+}
+
+function StoryTray({ stories }: { stories: Story[] }) {
+  const allStories = [...stories, { id: 'create', label: 'Create Story' }];
+
+  return (
+    <ul>
+      {allStories.map((story) => (
+        <li key={story.id}>{story.label}</li>
+      ))}
+    </ul>
+  );
+}
