295b8657dea63f7ee5b0e11cf6bf7cc2317644ca
AyanValiulin01 <ayangross@outlook.com>
2025-02-22 15:57:22 +0600

    Create app.tsx

---
+++ src/1/1_1_1/app.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <img
+            src="/AkliluLemma.jpg"  // Добавлен слеш перед именем файла
+            alt="Aklilu Lemma"
+        />
+    );
+}
+
+export default App;
